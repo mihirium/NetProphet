7dfc84ba5060ca9e24add3ea0aa64f535650f171
Mihir Sangameswar <mas6tm>
2023-03-26 04:28:06 -0400

    added team stats

---
+++ components/SpecificAwayTeam.js
@@ -0,0 +1,71 @@
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import React from "react";
+import Virginia from "../assets/teams/virginia.gif";
+import match_history from "../match_history.json";
+
+export default function SpecificTeamAway({ myTeam }) {
+  var dict = {};
+  dict["Atlanta Hawks"] = "Hawks";
+  dict["Boston Celtics"] = "Celtics";
+  dict["Brooklyn Nets"] = "Nets";
+
+  dict["Charlotte Hornets"] = "Hornets";
+  dict["Chicago Bulls"] = "Bulls";
+  dict["Cleveland Cavaliers"] = "Cavaliers";
+  dict["Dallas Mavericks"] = "Mavericks";
+  dict["Denver Nuggets"] = "Nuggets";
+  dict["Detroit Pistons"] = "Pistons";
+  dict["Golden State Warriors"] = "Warriors";
+  dict["Houston Rockets"] = "Rockets";
+  dict["Indiana Pacers"] = "Pacers";
+  dict["Los Angeles Clippers"] = "Clippers";
+  dict["Los Angeles Lakers"] = "Lakers";
+  dict["Memphis Grizzlies"] = "Grizzlies";
+  dict["Miami Heat"] = "miami_heat_logos";
+  dict["Milwaukee Bucks"] = "Bucks";
+  dict["Minnesota Timberwolves"] = "Timberwolves";
+  dict["New Orleans Pelicans"] = "Pelicans";
+  dict["New York Knicks"] = "Knicks";
+  dict["Oklahoma City Thunder"] = "Thunder";
+  dict["Orlando Magic"] = "Magic";
+  dict["Philadelphia 76ers"] = "76ers";
+  dict["Phoenix Suns"] = "Suns";
+  dict["Portland Trail Blazers"] = "Trailblazers";
+  dict["Sacramento Kings"] = "Kings";
+  dict["San Antonio Spurs"] = "Spurs";
+  dict["Toronto Raptors"] = "Raptors";
+  dict["Utah Jazz"] = "Jazz";
+  dict["Washington Wizards"] = "Wizards";
+
+  const TotPts = (team, gameData) => {
+    let totalHome3Ps = 0;
+    gameData.forEach((game) => {
+      if (game.Visitor === team) {
+        totalHome3Ps += game.Visitor_PTS;
+      }
+    });
+    return totalHome3Ps;
+  };
+  const ThreePA = (team, gameData) => {
+    let totalHome3Ps = 0;
+    let home3PA = 0;
+    gameData.forEach((game) => {
+      if (game.Visitor === team) {
+        totalHome3Ps += game.Visitor_3P;
+        home3PA += game.Visitor_3PA;
+      }
+    });
+    return Math.round((totalHome3Ps / home3PA) * 100);
+  };
+
+  return (
+    <View>
+      <Text style={{ fontSize: 20 }}> {dict[myTeam]}</Text>
+      <Text> Odds: 45%</Text>
+      <Text> Total Points: {TotPts(myTeam, match_history)}</Text>
+      <Text> 3 Point Average: {ThreePA(myTeam, match_history)}%</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- screens/GameDetails.js
+++ screens/GameDetails.js
@@ -1,13 +1,15 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
 import React from "react";
-import SpecificTeam from "../components/SpecificTeam";
+import SpecificTeamHome from "../components/SpecificTeamHome";
+import SpecificTeamAway from "../components/SpecificAwayTeam";
 import BackButton from "../components/BackArrow";
 import { useNavigation } from "@react-navigation/native";
 
 export default function GameDetails(props) {
   const navigation = useNavigation();
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.backgroundContainer}>
+      <Text style={styles.title}> Team Stats</Text>
       <View style={{ position: "absolute", top: 50, left: 20 }}>
         <BackButton
           onPress={() => {
@@ -15,17 +17,23 @@ export default function GameDetails(props) {
           }}
         />
       </View>
-      <SpecificTeam myTeam={props.route.params.teamOne} />
+      <View style={{ flexDirection: "row" }}>
+        <SpecificTeamHome myTeam={props.route.params.teamOne} />
+        <SpecificTeamAway myTeam={props.route.params.teamTwo} />
+      </View>
       {/* <Text style={{ fontSize: 30 }}>{route.params.teamOne}</Text> */}
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundContainer: {
     flex: 1,
     backgroundColor: "#F5F5F5",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
