0adf28a78e03bf00419781b04e9a70530b740ad0
TT <timmyltruong@gmail.com>
2023-03-26 04:51:46 -0400

    scores added

--- components/GameItem.js
+++ components/GameItem.js
@@ -10,6 +10,7 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import images from "../assets/b_logos/images.js";
 
+
 var dict = {};
 dict["Atlanta Hawks"] = "atlanta_hawks_logos";
 dict["Boston Celtics"] = "boston_celtics_logos";
@@ -43,7 +44,39 @@ dict["Toronto Raptors"] = "toronto_raptors_logos";
 dict["Utah Jazz"] = "utah_jazz_logos";
 dict["Washington Wizards"] = "washington_wizards_logos";
 
-export default function GameItem({ teamOne, teamTwo, date, start_time }) {
+var WLdict = {};
+WLdict["Milwaukee Bucks"] = [53, 20];
+WLdict["Boston Celtics"] = [51, 23];
+WLdict["Philadelphia 76ers"] = [49, 24];
+WLdict["Cleveland Cavaliers"] = [47, 28];
+WLdict["New York Knicks"] = [42, 33];
+WLdict["Miami Heat"] = [40, 34];
+WLdict["Brooklyn Nets"] = [39, 34];
+WLdict["Atlanta Hawks"] = [36, 37];
+WLdict["Toronto Raptors"] = [36, 38];
+WLdict["Chicago Bulls"] = [35, 38];
+WLdict["Indiana Pacers"] = [33, 41];
+WLdict["Washington Wizards"] = [33, 41];
+WLdict["Orlando Magic"] = [31, 43];
+WLdict["Charlotte Hornets"] = [24, 51];
+WLdict["Detroit Pistons"] = [16, 58];
+WLdict["Denver Nuggets"] = [49, 24];
+WLdict["Memphis Grizzlies"] = [46, 27];
+WLdict["Sacramento Kings"] = [44, 29];
+WLdict["Los Angeles Clippers"] = [39, 35];
+WLdict["Phoenix Suns"] = [38, 35];
+WLdict["Golden State Warriors"] = [39, 36];
+WLdict["Minnesota Timberwolves"] = [37, 37];
+WLdict["Los Angeles Lakers"] = [37, 37];
+WLdict["New Orleans Pelicans"] = [36, 37];
+WLdict["Oklahoma City Thunder"] = [36, 38];
+WLdict["Dallas Mavericks"] = [36, 38];
+WLdict["Utah Jazz"] = [35, 38];
+WLdict["Portland Trail Blazers"] = [32, 41];
+WLdict["San Antonio Spurs"] = [19, 55];
+WLdict["Houston Rockets"] = [18, 56];
+
+export default function GameItem({ teamOne, teamTwo , date, start_time}) {
   const navigation = useNavigation();
   return (
     <TouchableOpacity
@@ -59,20 +92,26 @@ export default function GameItem({ teamOne, teamTwo, date, start_time }) {
           <View>
             <View style={styles.row}>
               <View style={styles.imageContainer}>
-                <Image source={images[dict[teamOne]]} style={styles.image} />
+                <Image
+                  source={images[dict[teamOne]]}
+                  style={styles.image}
+                />
               </View>
               <View>
                 <Text style={styles.teamName}>{teamOne}</Text>
-                <Text style={styles.teamScore}>12-1</Text>
+                <Text style={styles.teamScore}>{WLdict[teamOne][0]}-{WLdict[teamOne][1]}</Text>
               </View>
             </View>
             <View style={styles.row}>
               <View style={styles.imageContainer}>
-                <Image source={images[dict[teamTwo]]} style={styles.image} />
+                <Image
+                  source={images[dict[teamTwo]]}
+                  style={styles.image}
+                />
               </View>
               <View>
                 <Text style={styles.teamName}>{teamTwo}</Text>
-                <Text style={styles.teamScore}>12-1</Text>
+                <Text style={styles.teamScore}>{WLdict[teamTwo][0]}-{WLdict[teamTwo][1]}</Text>
               </View>
             </View>
           </View>
@@ -81,10 +120,7 @@ export default function GameItem({ teamOne, teamTwo, date, start_time }) {
             <Text> {teamOne} 45% </Text>
           </View>
         </View>
-        <Text style={styles.time}>
-          {" "}
-          Time: {start_time} Date: {date}{" "}
-        </Text>
+        <Text style={styles.time}> Time: {start_time} Date: {date} </Text>
       </View>
       {/* <Button
         title="teamOne"
